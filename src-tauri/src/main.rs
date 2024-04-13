d096da8f127a7026207e9c58e28cd3a736e75dc4
Diana Kozachenko <d.kozachenko@tinkoff.ru>
2024-04-13 17:54:20 +0300

    chore(native): added logger

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,17 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
+    let var = std::env::var("DIR").unwrap();
+    log::info!("{}", var);
+
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
 fn main() {
+    simple_logger::init().unwrap();
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
